009a6577aaee56d6e3e56b8e56265cbb1b84fda2
tuan13501 <65405048+tuan13501@users.noreply.github.com>
2020-06-11 23:14:08 +0700

    Add files via upload
    Homework day 8

---
+++ script.js
@@ -0,0 +1,12 @@
+// console.log("Hello my name is ONE");
+// setTimeout(function(){ console.log("Hello my name is TWO"); }, 3000);
+// console.log("Hello my name is THREE");
+// function wait(ms) {
+//     return new Promise(r => setTimeout(r, ms))
+// }
+// async function main() {
+//     console.log("ONE");
+//     await wait(2000);
+//     console.log("TWO");
+// };
+// main();
